1ac2325c1cf4f1a9ef0768bca5f4bf282d78a773
Rob Wallator <rob17macbook@MacbookPro17.local>
2020-08-04 22:35:18 -0600

    finish confirm add error

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import "components/Appointment/Header"
 import "components/Appointment/Empty"
 import "components/Appointment/Confirm"
 import "components/Appointment/Show"
+import "components/Appointment/Status"
 
 
 export default function Appointment() {
--- stories/index.js
+++ stories/index.js
@@ -14,7 +14,9 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Confirm from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 
@@ -148,4 +150,11 @@ storiesOf("Button", module)
       message="Delete the appointment?"
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
+  />)
+  .add("Status", () => <Status
+      message="Deleting"
+  />)
+  .add("Error", () => <Error
+      message="Could not delete appointment."
+      onClose={action ("onClose")}
   />)
