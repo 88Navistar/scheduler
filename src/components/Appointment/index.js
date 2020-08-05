348ff4d5047a706fd0755e26b06e7797d11f9f37
Rob Wallator <rob17macbook@MacbookPro17.local>
2020-08-04 22:24:46 -0600

    add show working on confirm

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import "components/Appointment/styles.scss";
 import "components/Appointment/Header"
 import "components/Appointment/Empty"
+import "components/Appointment/Confirm"
+import "components/Appointment/Show"
+
 
 export default function Appointment() {
 
