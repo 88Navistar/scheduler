c5d0ede39882944cac5e0a40617fbdc5715c5055
Rob Wallator <rob17macbook@MacbookPro17.local>
2020-08-06 13:40:35 -0600

    get appointments for day helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,23 @@
 
+// function selectUserByName(state, name) {
+//   const filteredNames = state.users.filter(user => user.name === name);
+//   return filteredNames;
+// }
 
 export default function getAppointmentsForDay(state, day) {
-  
+  if (state.days.length === 0) {
+    return [];
+  }
+  const filterdedDays = state.days.filter(singleDay => {
+    return singleDay.name === day;
+  });
+
+  if (filterdedDays.length === 0) {
+    return [];
+  }
+  const dayAppt = filterdedDays[0].appointments;
+  const appointments = dayAppt.map(appt => {
+    return state.appointments[appt]
+  })
+  return appointments;
 }
